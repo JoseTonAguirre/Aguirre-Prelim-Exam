d29539c30b01d8abd7c381bd1e0f430250f4934a
Jose TonTon <joseanthonyaguirre@gmail.com>
2021-03-16 15:40:48 +0800

    Student has been addded

---
+++ Student.java
@@ -0,0 +1,49 @@
+class Student
+{
+    // the private data members
+    private int IDnumber;
+    private int hours;
+    private int points;
+
+    Student()
+    {
+        IDnumber = 9999;
+        points = 12;
+        hours = 3;
+    }
+
+
+    // the public get and set methods
+    public void setIDnumber(int number)
+    {
+        IDnumber = number;
+    }
+
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+    // methods to display the fields
+    public void showIDnumber()
+    {
+        System.out.println("ID Number is: " + IDnumber);
+    }
+
+    public void showHours()
+    {
+        System.out.println("Credit Hours: " + hours);
+    }
+
+    public void showPoints()
+    {
+        System.out.println("Points Earned: " + points);
+    }
+
+    public double getGradePoint()
+    {
+        return (double)points / hours;
+    }
+
+}
